8ac02bda03b48dcb841f0eeea66ee0de0b47ad6c
Lysenko Oleg <moto.froloff@yandex.ru>
2021-08-25 17:52:46 +0300

    Finish

--- Randomaiser/MainViewController.swift
+++ Randomaiser/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     
     @IBOutlet var minimumValueLabel: UILabel!
@@ -21,11 +21,23 @@ class ViewController: UIViewController {
         getRandomNumberBotton.layer.cornerRadius = 10
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.minimumValue = minimumValueLabel.text
+        settingsVC.maximumValue = maximumValueLabel.text
+    }
+    
     @IBAction func getRandomNumberButtonTapped() {
         let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
         let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
     
         randomValueLabel.text = String(Int.random(in: minimumNumber...maximumNumber))
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingsVC = segue.source as? SettingsViewController else { return }
+        minimumValueLabel.text = settingsVC.minimumValueTF.text
+        maximumValueLabel.text = settingsVC.maximumValueTF.text
     }
 }
 
