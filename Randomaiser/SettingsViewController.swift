b120c8c5f9503ce02fc1ba807ce8e9a0953b6d7c
Lysenko Oleg <moto.froloff@yandex.ru>
2021-08-25 16:54:06 +0300

    Adding SettingsViewController

---
+++ Randomaiser/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  Randomaiser
+//
+//  Created by Олег Лысенко on 25.08.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
