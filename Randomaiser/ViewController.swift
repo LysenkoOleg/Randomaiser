453051b076c61404d2380d4107e6ce9c14c8206c
Lysenko Oleg <moto.froloff@yandex.ru>
2021-08-25 15:59:37 +0300

    Setup main screen viewcontroller

--- Randomaiser/ViewController.swift
+++ Randomaiser/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    @IBOutlet var getRandomNumberBotton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getRandomNumberBotton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func getRandomNumberButtonTapped() {
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
+    
+        randomValueLabel.text = String(Int.random(in: minimumNumber...maximumNumber))
+    }
 }
 
